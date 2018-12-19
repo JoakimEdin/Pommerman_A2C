707bee50ebd9d1a6b3977b4e551fed68c69eb87d
s144323 <s144323@student.dtu.dk>
2018-12-19 15:39:29 +0100

    Jupyter notebook now functions

--- pommerman/agents/__init__.py
+++ pommerman/agents/__init__.py
@@ -5,6 +5,3 @@ from .http_agent import HttpAgent
 from .player_agent import PlayerAgent
 from .random_agent import RandomAgent
 from .simple_agent import SimpleAgent
-from .tensorforce_agent import TensorForceAgent
-from .pytorch_agent_reinforced import PytorchAgent_Reinforced
-from .actor_critic_agent import PytorchAgent_actor_critic
